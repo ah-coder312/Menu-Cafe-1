3fc3997898cfeedcefa3366b648d56d9293d7e7e
ahmed codes <ahmeedd2156@gmail.com>
2026-04-08 21:58:07 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,35 @@
+
+const menuPrices = {
+    // الساخن (Hot)
+    espresso: 160,
+    cappuccino: 190,
+    latte: 180,
+    caramel_macchiato: 200,
+    flat_white: 180,
+    hot_chocolate: 170,
+    hot_mocha: 190,
+    turkish_coffee: 160,
+    regular_coffee: 160,
+    tea: 120,
+
+    // البارد (Cold)
+    soda: 180,
+    ice_latte: 180,
+    spanish_latte: 190,
+    ice_caramel_macchiato: 200,
+    mojito: 150,
+    milkshake: 160,
+    smoothie: 140,
+    frappuccino: 190,
+    fresh_juice: 120,
+    water: 180,
+
+    // الحلويات (Dessert)
+    molten_cake: 120,
+    cheesecake: 150,
+    brownies: 130,
+
+    // الشيشة (Shisha)
+    shisha_qass: 20,
+    shisha_fruits: 80
+};
